06278d2e2a2094f624999b0bc7113fc45c61b058
Ciamuthama <muthamapeter79@gmail.com>
2023-07-27 13:17:53 +0300

    Added Routes to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from "react-bootstrap"
+import { Navigate, Route, Routes } from "react-router-dom"
+
 function App() {
 
   return (
     <>
-      <p>HI</p>
+      <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<p>Home</p>} />
+        <Route path="/create" element={<p>Create</p>} />
+        <Route path="/:id">
+          <Route index element={<h1>Details</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route> 
+        <Route path="*" element={<Navigate to='/'/>}/>
+    </Routes>
+      </Container>
+    
     </>
   )
 }
