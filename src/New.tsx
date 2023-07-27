951d2f12c1306a0c5ea9a281eba3684fcdaf563f
Ciamuthama <muthamapeter79@gmail.com>
2023-07-27 15:27:10 +0300

    Added Form component to New page.

--- src/New.tsx
+++ src/New.tsx
@@ -1,3 +1,12 @@
+import { Form } from "./Form";
+
 export function New() {
-    
+    return (
+
+        <>
+            <h2 className="mb-4">Create New Note</h2>
+            <Form />
+        </>
+
+    )
 }
