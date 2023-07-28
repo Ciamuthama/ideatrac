5ea6b78eaecafec77ccf7c6a4884081e08b0bb3d
Ciamuthama <muthamapeter79@gmail.com>
2023-07-28 14:13:59 +0300

    Added buttons to NotesList component.

--- src/NotesList.tsx
+++ src/NotesList.tsx
@@ -1,6 +1,21 @@
+import { Button, Col, Row, Stack } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 function NotesList() {
     return (<>
-    <h1>Hello</h1>
+        <Row>
+            <Col>
+            <h1>Notes</h1>
+            </Col>
+            <Col xs='auto'>
+                <Stack gap={2} direction="horizontal">
+                    <Link to='/create'>
+                        <Button variant="primary">Create</Button>
+                    </Link>
+                    <Button variant="secondary">Edit</Button>
+                </Stack>
+            </Col>
+        </Row>
     </>);
 }
 
