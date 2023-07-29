78960276aa91506dccd6733a0896cde3000b8889
Ciamuthama <muthamapeter79@gmail.com>
2023-07-29 16:25:10 +0300

    Added className `Card`

--- src/NotesList.tsx
+++ src/NotesList.tsx
@@ -79,7 +79,7 @@ export default NotesList
 
 function NoteCard({ id, title, tags }: SimplifiedNote) {
     return (
-        <Card as={Link} to={`/${id}`}>
+        <Card as={Link} to={`/${id}`} className={`h-100 text-reset text-decoration-none`}>
             <Card.Body>
                 <Stack gap={2} className="align-items-center justify-content-center h-100">
                     <span className="fs-5">{ title}</span>
